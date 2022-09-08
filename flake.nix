eadb4fa1fc2f10feb28f3bf12db716fe389c94a5
CozyPenguin <coozypenguin@gmail.com>
2022-09-08 22:44:41 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Flake for dottor";
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; }; in rec {
+        packages.dottor = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "dottor";
+          version = "0.1.0";
+        
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.openssl ];
+        
+          meta = with pkgs.lib; {
+            description = "A simple dotfiles manager";
+            homepage = "https://github.com/CozyPenguin/dottor";
+            license = licenses.mit;
+            mainProgram = "dottor";
+          };
+        };
+        defaultPackage = packages.dottor;
+        apps.dottor = flake-utils.lib.mkApp { drv = packages.dottor; };
+        defaultApp = apps.dottor;
+      }
+    );
+}
