d66afb7dd0e79309a16e4ddb3b4b205d257654fa
CozyPenguin <coozypenguin@gmail.com>
2022-09-09 10:54:42 +0200

    add overlay to flake

--- flake.nix
+++ flake.nix
@@ -1,29 +1,32 @@
 {
   description = "Flake for dottor";
 
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; }; in rec {
-        packages.dottor = pkgs.rustPlatform.buildRustPackage rec {
-          pname = "dottor";
-          version = "0.1.0";
-        
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
-        
-          nativeBuildInputs = [ pkgs.pkg-config ];
-          buildInputs = [ pkgs.openssl ];
-        
-          meta = with pkgs.lib; {
-            description = "A simple dotfiles manager";
-            homepage = "https://github.com/CozyPenguin/dottor";
-            license = licenses.mit;
-            mainProgram = "dottor";
-          };
+  outputs = { self, nixpkgs, flake-utils }: {
+    overlay = final: prev: {
+      dottor = self.packages.dottor;
+    };
+  } // (flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import nixpkgs { inherit system; }; in rec {
+      packages.dottor = pkgs.rustPlatform.buildRustPackage rec {
+        pname = "dottor";
+        version = "0.1.0";
+      
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+      
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        buildInputs = [ pkgs.openssl ];
+      
+        meta = with pkgs.lib; {
+          description = "A simple dotfiles manager";
+          homepage = "https://github.com/CozyPenguin/dottor";
+          license = licenses.mit;
+          mainProgram = "dottor";
         };
-        defaultPackage = packages.dottor;
-        apps.dottor = flake-utils.lib.mkApp { drv = packages.dottor; };
-        defaultApp = apps.dottor;
-      }
-    );
+      };
+      defaultPackage = packages.dottor;
+      apps.dottor = flake-utils.lib.mkApp { drv = packages.dottor; };
+      defaultApp = apps.dottor;
+    }
+  ));
 }
